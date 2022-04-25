1b02a3dcdb9dfc26ab4e27a1ed518075c5774601
Nishad Islam <nishad.nik@gmail.com>
2022-04-25 09:59:28 +0600

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
